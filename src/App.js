94cc11c38b8b142c9673fd57ee6eb68230fb36d3
Rhys Jones <j11.rhys@gmail.com>
2017-05-22 06:52:02 -0600

    got grid to save changes; added dropdown

--- src/App.js
+++ src/App.js
@@ -1,51 +1,73 @@
-import React, { Component } from 'react';
+const React = require('react');
 import './App.css';
-import DataGrid from 'react-data-grid'
+const DataGrid = require('react-data-grid');
+var update = require('react-addons-update');
+const { Editors, Formatters } = require('react-data-grid-addons');
+const { DropDownEditor } = Editors;
+const { DropDownFormatter } = Formatters;
 
 var rows = [
-            {id:"1",firstName:"John",lastName:"Smith",city:"Podunk",email:"js@dim.com"},
-            {id:"2",firstName:"Jane",lastName:"Doe",city:"Nowhere",email:"jd@slimy.com"},
-            {id:"3",firstName:"Frog",lastName:"Toad",city:"Hicksville",email:"ft@zoomy.com"}
-            ]
+            {id:"1",firstName:"John",lastName:"Smith",city:"Podunk",
+                coolness:"cool",email:"js@dim.com"},
+            {id:"2",firstName:"Jane",lastName:"Doe",city:"Nowhere",
+                coolness:"uncool",email:"jd@slimy.com"},
+            {id:"3",firstName:"Frog",lastName:"Toad",city:"Hicksville",
+                coolness:"cool",email:"ft@zoomy.com"}
+            ];
 
-var columns = [
-  { key: 'id', name: 'id', width: 50 },
-  { key: 'firstName', name: 'firstName' },
-  { key: 'lastName', name: 'lastName'  },
-  { key: 'city', name: 'city' },
-  { key: 'email', name: 'email' }
-]
+const coolTypes = ['cool','uncool'];
+//  { id: 'cool', value: 'cool', text: 'Cool', title: 'Cool' },
+//  { id: 'uncool', value: 'uncool', text: 'Not Cool', title: 'Not Cool' }
+//];
+const CoolTypesEditor = <DropDownEditor options={coolTypes} />;
+const CoolTypesFormatter = <DropDownFormatter options={coolTypes} value="cool" />;
 
-const rowGetter = rowNumber => rows[rowNumber];
+const App = React.createClass({
+  getInitialState() {
+      this._columns = [
+      { key: 'id', name: 'ID', width: 50 },
+      { key: 'firstName', name: 'First Name', editable: true },
+      { key: 'lastName', name: 'Last Name', editable: true  },
+      { key: 'city', name: 'City', editable: true },
+      { key: 'coolness', name: "Cool?", editor: CoolTypesEditor,
+        formatter: CoolTypesFormatter },
+      { key: 'email', name: 'Email', editable: true}
+      ];
+  return { rows: rows };
+  },
 
-class App extends Component {
+  rowGetter(i) {
+    return this.state.rows[i];
+  },
+
+  handleGridRowsUpdated({ fromRow, toRow, updated }) {
+    let rows = this.state.rows.slice();
+
+    for (let i = fromRow; i <= toRow; i++) {
+      let rowToUpdate = rows[i];
+//      let updatedRow = React.addons.update(rowToUpdate, {$merge: updated});
+      let updatedRow = update(rowToUpdate, {$merge: updated});
+      rows[i] = updatedRow;
+    }
+
+    this.setState({ rows });
+  },
+
   render() {
     return (
       <DataGrid
       enableCellSelect={true}
-      idProperty='id'
-      dataSource={rows}
-      columns={columns}
-      rowGetter={rowGetter}
-      rowsCount={rows.length}
-      style={{height: 500}}
+      columns={this._columns}
+      rowGetter={this.rowGetter}
+      rowsCount={this.state.rows.length}
+      minHeight={500}
+      onGridRowsUpdated={this.handleGridRowsUpdated}
       //if you don't want to show a column menu to show/hide columns, use
       //withColumnMenu={false}
       />
-      /*
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-      */
-
     );
   }
-}
+})
 
 export default App;
+//module.exports = App
